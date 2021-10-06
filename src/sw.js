cb71513983b7c27785eb0d0f3d45ada1258886d1
Banda Bahari Putra <bandabahariputra99@gmail.com>
2021-10-06 09:02:15 +0800

    Complete service worker configuration

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,44 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { clientsClaim, skipWaiting } from 'workbox-core';
 
-precacheAndRoute(self.__WB_MANIFEST);
+registerRoute(
+  new RegExp('https://fonts.googleapis.com'),
+  new CacheFirst({ cacheName: 'google-apis' })
+);
+
+registerRoute(
+  new RegExp('https://fonts.gstatic.com'),
+  new CacheFirst({ cacheName: 'g-static' })
+);
+
+registerRoute(
+  new RegExp(/\.(?:eot|ttf|woff|woff2)$/),
+  new CacheFirst({ cacheName: 'font-icons' })
+);
+
+registerRoute(
+  new RegExp(/\.(?:css|js|png|jpg|svg|gif)$/),
+  new CacheFirst({ cacheName: 'assets' })
+);
+
+registerRoute(
+  new RegExp('/team.html'),
+  new StaleWhileRevalidate({ cacheName: 'pages' })
+);
+
+registerRoute(
+  new RegExp('https://api.football-data.org'),
+  new StaleWhileRevalidate({ cacheName: 'api' })
+);
+
+registerRoute(
+  new RegExp('https://crests.football-data.org'),
+  new StaleWhileRevalidate({ cacheName: 'emblem-team' })
+);
+
+skipWaiting();
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST, { ignoreURLParametersMatching: [/.*/] });
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,5 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'development',
-  watch: true
+  mode: 'development'
 });
