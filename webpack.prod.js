a8d36bb1847d05fbce85f1bee98fc53d31680112
Banda Bahari Putra <bandabahariputra99@gmail.com>
2021-10-12 22:23:43 +0800

    Service worker work in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,14 @@ module.exports = merge(common, {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    'targets': { esmodules: true }
+                  }
+                ]
+              ]
             }
           }
         ]
